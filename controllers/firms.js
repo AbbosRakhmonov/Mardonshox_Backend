d449018bd5d634099a0cf9dd817dd4da38534216
Rakhomonov <abbos.raxmonov.2001@gmail.com>
2022-10-16 16:49:39 +0500

    done

--- controllers/firms.js
+++ controllers/firms.js
@@ -1,82 +1,86 @@
-const ErrorResponse = require('../utils/errorResponse')
-const asyncHandler = require('../middleware/async')
-const Firm = require('../models/Firms')
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const Firm = require("../models/Firms");
 
 // @desc      Get all firms
-// @route     GET /api/v1/firms
+// @route     GET /api/v1/firms/:id
 // @access    Public
 exports.getAllFirms = asyncHandler(async (req, res, next) => {
-    const firms = await Firm.find()
-    res.status(200).json({
-        success: true,
-        count: firms.length,
-        data: firms
-    })
-})
+  const firms = await Firm.find({ user: req.params.id });
+  res.status(200).json({
+    success: true,
+    count: firms.length,
+    data: firms,
+  });
+});
 
 // @desc      Create new firm
 // @route     POST /api/v1/firms
 // @access    Private
 exports.createFirm = asyncHandler(async (req, res, next) => {
-    const publishedFirm = await Firm.findOne({name: req.body.name})
+  const publishedFirm = await Firm.findOne({ name: req.body.name });
 
-    if (publishedFirm) {
-        return next(
-            new ErrorResponse(
-                `The firm with the name ${req.body.name} already exists`,
-                400
-            )
-        )
-    }
+  if (publishedFirm) {
+    return next(
+      new ErrorResponse(
+        `The firm with the name ${req.body.name} already exists`,
+        400
+      )
+    );
+  }
 
-    const firm = await Firm.create(req.body)
+  const firm = await Firm.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        data: firm
-    })
-})
+  res.status(201).json({
+    success: true,
+    data: firm,
+  });
+});
 
 // @desc      Update firm
 // @route     PUT /api/v1/firms/:id
 // @access    Private
 exports.updateFirm = asyncHandler(async (req, res, next) => {
-    let name = req.body.name
-    const firm = await Firm.findByIdAndUpdate(req.params.id, {
-        name: (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()).trim()
-    }, {
-        new: true,
-        runValidators: true
-    })
-
-    console.log(firm)
-
-    if (!firm) {
-        return next(
-            new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
-        )
+  let name = req.body.name;
+  const firm = await Firm.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()).trim(),
+    },
+    {
+      new: true,
+      runValidators: true,
     }
+  );
 
-    res.status(200).json({
-        success: true,
-        data: firm
-    })
-})
+  console.log(firm);
+
+  if (!firm) {
+    return next(
+      new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: firm,
+  });
+});
 
 // @desc      Delete firm
 // @route     DELETE /api/v1/firms/:id
 // @access    Private
 exports.deleteFirm = asyncHandler(async (req, res, next) => {
-    const firm = await Firm.findByIdAndDelete(req.params.id)
+  const firm = await Firm.findByIdAndDelete(req.params.id);
 
-    if (!firm) {
-        return next(
-            new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
-        )
-    }
+  if (!firm) {
+    return next(
+      new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-    res.status(200).json({
-        success: true,
-        data: req.params.id
-    })
-})
+  res.status(200).json({
+    success: true,
+    data: req.params.id,
+  });
+});
--- routes/firms.js
+++ routes/firms.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const {protect} = require('../middleware/auth')
 const {createFirm, deleteFirm, getAllFirms, updateFirm} = require('../controllers/firms')
 
-router.route('/').get(getAllFirms).post(protect, createFirm)
-router.route('/:id').put(protect, updateFirm).delete(protect, deleteFirm)
+router.route('/').post(protect, createFirm)
+router.route('/:id').put(protect, updateFirm).delete(protect, deleteFirm).get(protect, getAllFirms)
 
 module.exports = router
 
