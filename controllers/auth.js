2a8e04e72880b0f742579b4a04e6bce9a2d02eed
Rakhomonov <abbos.raxmonov.2001@gmail.com>
2022-10-19 17:41:49 +0500

    jwt expire 6 hours

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,88 +1,89 @@
-const ErrorResponse = require('../utils/errorResponse')
-const asyncHandler = require('../middleware/async')
-const User = require('../models/User')
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
 
 // @desc      Register user
 // @route     POST /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-    // Create user
-    const user = await User.create({
-        name: req.body.name,
-        login: req.body.login,
-        password: req.body.password
-    })
+  // Create user
+  const user = await User.create({
+    name: req.body.name,
+    login: req.body.login,
+    password: req.body.password,
+  });
 
-    sendTokenResponse(user, 200, res)
-})
+  sendTokenResponse(user, 200, res);
+});
 
 // @desc      Login user
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
-    const {login, password} = req.body
+  const { login, password } = req.body;
 
-    // Validate email & password
-    if (!login || !password) {
-        return next(new ErrorResponse('Please provide an login and password', 400))
-    }
+  // Validate email & password
+  if (!login || !password) {
+    return next(new ErrorResponse("Please provide an login and password", 400));
+  }
 
-    // Check for user
-    const user = await User.findOne({login}).select('+password')
-    const resUser = await User.findOne({login})
+  // Check for user
+  const user = await User.findOne({ login }).select("+password");
+  const resUser = await User.findOne({ login });
 
-    if (!user) {
-        return next(new ErrorResponse('Логин ёки парол хато !', 401))
-    }
+  if (!user) {
+    return next(new ErrorResponse("Логин ёки парол хато !", 401));
+  }
 
-    // Check if password matches
-    const isMatch = await user.matchPassword(password)
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
 
-    if (!isMatch) {
-        return next(new ErrorResponse('Invalid credentials', 401))
-    }
+  if (!isMatch) {
+    return next(new ErrorResponse("Логин ёки парол хато !", 401));
+  }
 
-    sendTokenResponse(user, 200, res, resUser)
-})
+  sendTokenResponse(user, 200, res, resUser);
+});
 
 // @desc      Update login and password
 // @route     PUT /api/v1/auth/update
 // @access    Private
 exports.updateLoginAndPassword = asyncHandler(async (req, res, next) => {
-    const user = await User.findByIdAndUpdate(req.user.id, req.body, {
-        new: true,
-        runValidators: true
-    })
+  const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!user) {
-        return next(
-            new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
-        )
-    }
+  if (!user) {
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-    res.status(200).json({
-        success: true,
-        data: user
-    })
-})
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
 const sendTokenResponse = (user, statusCode, res, resUser) => {
-    // Create token
-    const token = user.getSignedJwtToken()
+  // Create token
+  const token = user.getSignedJwtToken();
 
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
-    }
+  // JWT options expires in 6 hours
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
 
-    if (process.env.NODE_ENV === 'production') {
-        options.secure = true
-    }
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
 
-    res.status(statusCode).cookie('token', token, options).json({
-        success: true,
-        token
-    })
-}
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
--- models/User.js
+++ models/User.js
@@ -1,63 +1,63 @@
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        trim: true
-    },
-    login: {
-        type: String,
-        required: [true, 'Please add a login'],
-        unique: true,
-        trim: true,
-        select: false
-    },
-    password: {
-        type: String,
-        required: [true, 'Please add a password'],
-        select: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    twoFactorCode: String,
-    twoFactorCodeExpire: Date,
-    twoFactorEnable: {
-        type: Boolean,
-        default: false
-    },
-    firm: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Firm'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    trim: true,
+  },
+  login: {
+    type: String,
+    required: [true, "Please add a login"],
+    unique: true,
+    trim: true,
+    select: false,
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  twoFactorCode: String,
+  twoFactorCodeExpire: Date,
+  twoFactorEnable: {
+    type: Boolean,
+    default: false,
+  },
+  firm: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Firm",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next()
-    }
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
-    return jwt.sign({id: this._id, name: this.name}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE
-    })
-}
+  return jwt.sign({ id: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema);
