d183c49899e0a453f2bacc2b0ce8e822cb819497
Rakhomonov <abbos.raxmonov.2001@gmail.com>
2022-11-27 02:08:38 +0500

    some changes

--- controllers/firms.js
+++ controllers/firms.js
@@ -1,17 +1,37 @@
-const ErrorResponse = require("../utils/errorResponse");
-const asyncHandler = require("../middleware/async");
-const Firm = require("../models/Firms");
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
+const Firm = require('../models/Firms')
+const Report = require('../models/Reports')
+
+const calculateTotal = async (reports) => {
+  let allIncomes = 0
+  let allOutcomes = 0
+  reports.forEach((report) => {
+    allIncomes += report.income
+    allOutcomes += report.outcome
+  })
+  return {
+    incomes: allIncomes,
+    outcomes: allOutcomes,
+    all: allOutcomes - allIncomes
+  }
+}
 
 // @desc      Get all firms
 // @route     GET /api/v1/firms/:id
 // @access    Public
-exports.getAllFirms = asyncHandler(async (req, res, next) => {
-  const firms = await Firm.find({ user: req.params.id });
+exports.getAllFirms = asyncHandler(async (req, res) => {
+  const firms = await Firm.find({user: req.params.id})
+
+  const allReports = await Report.find({firm: {$in: firms}})
+  const all = await calculateTotal(allReports)
+
   res.status(200).json({
     success: true,
     count: firms.length,
     data: firms,
-  });
+    ...all
+  })
 });
 
 // @desc      Create new firm
@@ -71,16 +91,20 @@ exports.updateFirm = asyncHandler(async (req, res, next) => {
 // @route     DELETE /api/v1/firms/:id
 // @access    Private
 exports.deleteFirm = asyncHandler(async (req, res, next) => {
-  const firm = await Firm.findByIdAndDelete(req.params.id);
-
+  const firm = await Firm.findByIdAndDelete(req.params.id)
+  const {user} = req.body
   if (!firm) {
     return next(
-      new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
-    );
+        new ErrorResponse(`Firm not found with id of ${req.params.id}`, 404)
+    )
   }
+  const firms = await Firm.find({user: user})
+  const allReports = await Report.find({firm: {$in: firms}})
+  const all = await calculateTotal(allReports)
 
   res.status(200).json({
     success: true,
     data: req.params.id,
-  });
+    ...all
+  })
 });
